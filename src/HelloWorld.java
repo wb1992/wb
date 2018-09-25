241f832b36d0a5aa3fb0f4ef3f511305a5662c60
wb <cd01234567899@163.com>
2018-09-25 21:26:48 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
